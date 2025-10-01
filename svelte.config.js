95221505d9c53e2381b22b9ca28b96874af56fb2
Thomas <thomas.marxsen11@gmail.com>
2025-09-30 23:53:33 -0500

    Pages working

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,15 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			fallback: '404.html',
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+const dev = process.argv.includes('dev');
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/DBF_Calculations',
+    },
+    appDir: 'internal',
+  }
 };
-
-console.log(process.argv.includes('dev') ? '' : process.env.BASE_PATH);
-
-export default config;
